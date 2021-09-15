24c9a0912178b3b03f514a6a0aeb153eb9814a4a
Lee Hoseop <ghtjq3909@naver.com>
2021-09-15 21:07:57 +0900

    Do refactor

--- Level 2/방금그곡.py
+++ Level 2/방금그곡.py
@@ -7,7 +7,14 @@ def timeCalc(start, end):
     
     return end - start
     
-def soundTransform(soundmap, sound):
+def soundTransform(sound):
+    soundmap = {'A#': '0',
+                'B#': '1',
+                'C#': '2',
+                'D#': '3',
+                'E#': '4',
+                'F#': '5',
+                'G#': '6'}
     for before, after in soundmap.items():
         sound = sound.replace(before, after)
     
@@ -22,19 +29,12 @@ def getSong(time, sound):
 def solution(m, musicinfos):
     answer = '(None)'
     longestTime = 0
-    soundmap = {'A#': '0',
-                'B#': '1',
-                'C#': '2',
-                'D#': '3',
-                'E#': '4',
-                'F#': '5',
-                'G#': '6'}
-    m = soundTransform(soundmap, m)
+    m = soundTransform(m)
     
     for musicinfo in musicinfos:
         start, end, name, sound = musicinfo.split(",")
         playTime = timeCalc(start, end)
-        sound = soundTransform(soundmap, sound)
+        sound = soundTransform(sound)
         sound = getSong(playTime, sound)
         
         if m in sound and playTime > longestTime:
